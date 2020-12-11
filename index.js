6abafee4099ec4a17699c14ca85b9f47f0da7a14
madhankumar2211 <madhankumar199722@gmail.com>
2020-12-11 11:58:31 +0530

    npm file

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require("fs")
+
+const example = fs.readFileSync('welcome.txt') //sync
+console.log(example.toString());
+
+fs.readFile('welcome.txt', function (error, value) { //async
+    if (error) {
+        return console.log(error);
+    }
+    console.log("succesfull", value.toString());//ascii
+})
+
+fs.writeFile('welcome.txt', 'hello World', function (error) {
+    if (error) throw error;
+    console.log('changed');
+})
+
+//file system
+console.log("hello");
